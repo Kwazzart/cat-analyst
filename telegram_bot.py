cf0799bbeda07c43a7908b3ef06bb6dce3295850
Дмитрий Сахаров <dmitrysakharov@MacBook-Air-Dmitrij.local>
2022-10-14 15:32:25 +0300

    enviro

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,23 +1,5 @@
-import json
-from telegram import Update
-from telegram.ext import Updater, CallbackContext, TypeHandler
+import telegram
+TOKEN = '5554474456:AAH59s3E6GIngfY6KJKIqsUue8ON-JO5qa8'
+bot = telegram.Bot(token=TOKEN)
+print(bot.get_me())
 
-
-def echo(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(json.dumps(update.to_dict(), indent=2))
-
-
-def main() -> None:
-    updater = Updater("5554474456:AAH59s3E6GIngfY6KJKIqsUue8ON-JO5qa8")
-
-    updater.dispatcher.add_handler(TypeHandler(Update, echo))
-
-    updater.start_polling()
-
-    print('Started')
-
-    updater.idle()
-
-
-if __name__ == "__main__":
-    main()
