f0c817bf5f76193f950e1a604f2e9ae984ae7cf5
kwazzart <vladimirkwaz@yandex.ru>
2022-10-17 21:08:47 +0300

    reset constants

--- scripts/Constants.py
+++ scripts/Constants.py
@@ -1,12 +1,4 @@
 import os
 
 TOKEN = '5554474456:AAH59s3E6GIngfY6KJKIqsUue8ON-JO5qa8'
-
 DATA_URL = str(os.getcwd())
-DATA_URL = DATA_URL.split("\\")
-url = ""
-for el_url in DATA_URL:
-    if el_url == "cat-analyst":
-        break
-    url += f'{el_url}\\'
-DATA_URL = url[:-1]
