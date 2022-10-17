9167fa58a4fc8453481c9bd040a96a0df4a10305
kwazzart <vladimirkwaz@yandex.ru>
2022-10-17 15:13:12 +0300

    Change main.py pipleline + constants file

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-#importing
+#imports
+from asyncore import dispatcher
+import Constants
 import logging
-import telegram
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
-from telegram.ext import MessageHandler, Filters
 
-#main variables
-TOKEN = '5554474456:AAH59s3E6GIngfY6KJKIqsUue8ON-JO5qa8'
-updater = Updater(token = TOKEN)
-dispatcher = updater.dispatcher
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+#logs activate
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                    level=logging.INFO)
 
@@ -16,16 +14,11 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 def start(update, context): 
     context.bot.send_message(chat_id=update.effective_chat.id, 
                              text="иди нах)")
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-
-
+    
 def echo(update, context):
     text = 'ладно все заткнись'
     context.bot.send_message(chat_id=update.effective_chat.id, 
                              text=text)
-echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-dispatcher.add_handler(echo_handler)
 
 def caps(update, context):
     if context.args:
@@ -38,10 +31,14 @@ def caps(update, context):
         context.bot.send_message(chat_id=update.effective_chat.id, 
                                 text='send: /caps argument')
 
-#create handler
-caps_handler = CommandHandler('caps', caps)
-dispatcher.add_handler(caps_handler)
+if __name__ == "__main__":
+    
+    updater = Updater(Constants.TOKEN)
+    dispatcher = updater.dispatcher
+    
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CommandHandler('caps', caps))
+    dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), echo))
 
-#main func
-updater.start_polling()
+    updater.start_polling()
 
