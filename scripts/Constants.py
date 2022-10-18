1df16803bf73d627c426f5c37e154ba2991bcae6
Dimas) <sahaapleid@yandex.ru>
2022-10-18 18:03:51 +0300

    small_update

--- scripts/Constants.py
+++ scripts/Constants.py
@@ -1,8 +1,12 @@
 import os
 
 TOKEN = '5554474456:AAH59s3E6GIngfY6KJKIqsUue8ON-JO5qa8'
-DATA_URL = str(os.getcwd())
-
+#FOR VOVA 
+DATA_URL = str(os.getcwd()) 
+#FOR DIMA
+#DATA_URL = '/Users/dmitrysakharov/Documents/Bot_analyst' 
 ID = None
 DATA = None
 DATA_VARS = None
+
+
